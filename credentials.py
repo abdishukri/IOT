2ed7ed86faa02697df4efffd32700a0b5c03bd22
dabato <faras85@hotmail.com>
2023-11-04 17:59:48 +0100

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -10,7 +10,7 @@ credentials = {
     'ssid' : 'LTE-1485',
     'password' : '1234567890',
     'ADAFRUIT_IO_URL' : b'io.adafruit.com',
-    'ADAFRUIT_USERNAME' : b'abdi001',
-    'ADAFRUIT_IO_KEY' : b'aio_emfg41TmZTf4x7odrUw4VOw7rUHW',
-    'ADAFRUIT_IO_FEEDNAME' : b'credent'
+    'ADAFRUIT_USERNAME' : b'',
+    'ADAFRUIT_IO_KEY' : b'',
+    'ADAFRUIT_IO_FEEDNAME' : b''
     }
